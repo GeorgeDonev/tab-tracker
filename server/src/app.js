9c6524a683b789f9fc63dfd517d2edd6df94de5f
GeorgeDonev <georgesdonev@gmail.com>
2018-02-09 15:22:40 -1000

    /server/scr/app.js building express server && nodemon server fixed (removed eslint, use code-reforctor via JSStandard in IDE)

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,14 @@
-
 console.log('hello there')
+
+//import the node modules by setting them as constant values
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+//build up the express server, very basic
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
